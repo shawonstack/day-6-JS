f31f7324818b701b48806dc12a852dcb77a09dd8
shawonstack <shawon934764@gmail.com>
2026-06-02 23:35:48 +0600

    add

---
+++ parameter.js
@@ -0,0 +1,9 @@
+function orderFood(foodName) {
+  console.log(`Preparing ${foodName}`);
+}
+// multiple perameter
+function userInfo(name, age, city) {
+  console.log(name);
+  console.log(age);
+  console.log(city);
+}
