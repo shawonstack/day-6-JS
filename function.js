1c4291d2c53cdd6156812e6e93c85d40379a028f
shawonstack <shawon934764@gmail.com>
2026-06-02 18:33:53 +0600

    fnction declare basics complete using so many real topis

--- function.js
+++ function.js
@@ -1,1 +1,49 @@
-console.log('hi');
+function sayHello() {
+  console.log('helow world');
+}
+
+function checking() {
+  console.log('checking email');
+  console.log('checking  password');
+  console.log('checking  phon numer');
+}
+
+function startEngine() {
+  console.log('engine startng');
+}
+function dirving() {
+  console.log('car driving');
+}
+
+function loginFunction() {
+  checkPass();
+}
+function checkPass() {
+  console.log('password checked');
+}
+loginFunction();
+
+function sayMyName() {
+  console.log('My name is Blood');
+}
+function showCountry() {
+  console.log('Bangladesh');
+}
+function welcome() {
+  console.log('Welcome to javascript');
+}
+function startGame() {
+  console.log('Start Game');
+}
+function endGame() {
+  console.log('Game Ended');
+}
+function wakeUp() {
+  console.log('wake Up');
+}
+function eatBreakfast() {
+  console.log('Eat Breakfast');
+}
+function goOffice() {
+  console.log('Go Office');
+}
