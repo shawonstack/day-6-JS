3cd02634ad560bfa3bbdd0830f8879419bc5e1b1
shawonstack <shawon934764@gmail.com>
2026-06-02 23:24:49 +0600

    task done

--- expression.js
+++ expression.js
@@ -42,3 +42,25 @@ const mother = () => {
   console.log('my mother is the most imortant person in the world for me');
 };
 // mother();
+
+// একটা Function Expression বানা যা ৩টা number receive করে তাদের average return করবে।
+const avg = (a, b, c) => {
+  let sum = a + b + c;
+  let average = sum / 3;
+
+  console.log('sum:', sum);
+
+  return average;
+};
+
+console.log('average:', avg(10, 20, 30));
+
+// একটা Function Expression বানা যা number even হলে: even and odd hole odd
+const numberChecking = num => {
+  if (num % 2 === 0) {
+    console.log('this is even number', num);
+  } else {
+    console.log('this is odd number', num);
+  }
+};
+numberChecking(12);
