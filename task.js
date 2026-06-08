6582c7fb5e52bc1e3f920e54e92db3529af6f244
shawonstack <shawon934764@gmail.com>
2026-06-08 23:45:55 +0600

    task5 completed function rleted issue

---
+++ task.js
@@ -0,0 +1,27 @@
+// task 1
+function greet(name) {
+  return `hello ${name}`;
+}
+const result = greet('shawon');
+console.log(result);
+// task 2
+const bioInfo = () => {
+  let name = 'shawon';
+  return `hello ${name}`;
+};
+console.log(bioInfo());
+// task 3
+const biggestNumberCheck = (a, b) => {
+  if (a > b) {
+    return `the biggest number is ${a}`;
+  } else {
+    return `biggest number is ${b}`;
+  }
+};
+console.log(biggestNumberCheck(222, 2));
+// task 4
+
+// task 5
+// task 6
+// task 7
+// task 8
