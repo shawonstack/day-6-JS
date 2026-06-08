3e4f02ccd44c8d4d5a1f83fe95d485f18e73355e
shawonstack <shawon934764@gmail.com>
2026-06-08 08:59:50 +0600

    return releted mini probblem solved discount passwrd false true

--- return.js
+++ return.js
@@ -2,15 +2,15 @@
 //   return a + b;
 // }
 
-// const result = add(4, 5);
-// // console.log(result);
-// function add(a, b) {
-//   console.log(a + b);
-// }
+const result = add(4, 5);
+// console.log(result);
+function add(a, b) {
+  console.log(a + b);
+}
 
-// const result1 = add(5, 10);
+const result1 = add(5, 10);
 
-// console.log(result1);
+console.log(result1);
 
 // ---------
 function test() {
@@ -19,3 +19,27 @@ function test() {
   console.log('b');
 }
 test();
+// -----------
+function chckAge(age) {
+  if (age >= 18) {
+    return 'adult';
+  }
+  return 'minor';
+}
+const res = chckAge(12);
+console.log(res);
+------
+function verifyPassword(pass) {
+  if (pass === 12345) {
+    return true;
+  }
+  return false;
+}
+const res = verifyPassword(12345);
+console.log(res);
+// ----------
+function calculateDiscount(price) {
+  return price * 0.1;
+}
+const discount = calculateDiscount(1000);
+console.log(discount);
