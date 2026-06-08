f9b4aecb20889bc05f10dda35827cb16a8a5ae04
shawonstack <shawon934764@gmail.com>
2026-06-08 20:26:35 +0600

    function scope, global scope and local scope releted isse fix

---
+++ scope.js
@@ -0,0 +1,22 @@
+// scope releted
+
+function room() {
+  let name = 'shawon';
+  console.log(name);
+}
+// room();
+function calculateSalary() {
+  let salary = 40000;
+  let bonus;
+  console.log(salary);
+}
+// calculateSalary();
+let name = 'Global';
+
+function show() {
+  let name = 'Local';
+
+  console.log(name);
+}
+
+show();
